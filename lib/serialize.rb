3dfafb280b91990d9c47d8737b1dd7de6c2a0ba6
Guy van den Berg <vandenberg.guy@gmail.com>
2008-03-23 11:13:40 +0200

    moved serialization to dm-more

---
+++ lib/serialize.rb
@@ -0,0 +1,102 @@
+require Pathname('rexml/document')
+
+begin
+  require 'faster_csv'
+rescue LoadError
+  puts "Counld not load FASTER_CSV"
+  nil
+end
+
+begin
+  require Pathname('json/ext')
+rescue LoadError
+  require Pathname('json/pure')
+end
+
+module DataMapper
+  module Serialize
+        
+    # Serialise a resource to YAML
+    #      
+    def to_yaml(opts = {})
+      YAML::quick_emit(object_id,opts) do |out|
+        out.map(nil,to_yaml_style) do |map|
+          self.class.properties(self.repository.name).each do |property|
+            value = self.send(property.name.to_sym)
+            map.add(property.name, value.is_a?(Class) ? value.to_s : value)              
+          end   
+          (self.instance_variable_get("@yaml_addes") || []).each do |k,v|
+            map.add(k.to_s,v)
+          end
+        end
+      end
+    end
+    
+    # Serialise a resource to XML
+    #
+    def to_xml(opts = {})
+      to_xml_document(opts).to_s
+    end
+    
+    # Return the name of this resource - to be used as the root element
+    # name. This can be overloaded
+    #
+    def xml_element_name 
+      Inflector.underscore(self.class.name)
+    end
+    
+    # Return a REXML::Document representing this resource
+    #
+    def to_xml_document(opts={}) 
+      doc = REXML::Document.new
+      root = doc.add_element(xml_element_name)
+      keys = self.class.key(self.repository.name)
+      keys.each do |key|
+        value = self.send(key.name)
+        root.attributes[key.name.to_s] = value
+      end
+      
+      #TODO old code base was converting single quote to double quote on attribs
+    
+      self.class.properties(self.repository.name).each do |property|
+        if !keys.include?(property)
+          value = self.send(property.name)
+          node = root.add_element(property.name.to_s)
+          node << REXML::Text.new(value.to_s) unless value.nil?
+        end          
+      end                     
+      doc
+    end
+    
+    def to_json
+      result = '{ '
+      fields = []
+      self.class.properties(self.repository.name).each do |property|
+        fields << "#{property.name.to_json}: #{self.send(property.name).to_json}"
+      end
+      result << fields.join(', ')      
+      result << ' }'
+      result
+    end
+    
+    def to_csv(writer = '')
+      FasterCSV.generate(writer) do |csv|
+        row = []
+        self.class.properties(self.repository.name).each do |property|
+         row << self.send(property.name).to_s            
+        end
+        csv << row
+#        csv << self.class.properties(self.repository.name).each {|property| self.send(property.name).to_s}            
+      end
+    end
+    
+    #def to_csv(writer = "")
+    #  FasterCSV.generate(writer) do |csv|
+    #    csv << database_context.table(self.class).columns.map { |column| get_value_for_column(column) }
+    #  end
+    #  return writer
+    #end
+
+
+  end
+end # module DataMapper
---
+++ spec/serialize_spec.rb
@@ -0,0 +1,78 @@
+require 'pathname'
+require Pathname(__FILE__).dirname.expand_path + 'spec_helper'
+require Pathname(__FILE__).dirname.parent.expand_path + 'lib/serialize'
+
+
+describe DataMapper::Serialize do
+
+  before(:all) do
+    class Cow
+      include DataMapper::Resource    
+      include DataMapper::Serialize
+      property :id, Fixnum, :key => true
+      property :composite, Fixnum, :key => true
+      property :name, String
+      property :breed, String
+    end
+  end
+  
+  
+  it "should serialize a resource to YAML" do
+    betsy = Cow.new
+    betsy.id = 230
+    betsy.composite = 22
+    betsy.name = 'Betsy'
+    betsy.breed = 'Jersey'
+    betsy.to_yaml.strip.should == <<-EOS.margin
+      --- 
+      :id: 230
+      :composite: 22
+      :name: Betsy
+      :breed: Jersey
+    EOS
+  end
+  
+  it "should serialize a resource to XML" do
+    berta = Cow.new
+    berta.id = 89
+    berta.composite = 34
+    berta.name = 'Berta'
+    berta.breed = 'Guernsey'
+    
+    berta.to_xml.should == <<-EOS.compress_lines(false)
+      <cow composite='34' id='89'>
+        <name>Berta</name>
+        <breed>Guernsey</breed>
+      </cow>
+    EOS
+  end
+  
+  it "should serialize a resource to JSON" do
+    harry = Cow.new
+    harry.id = 1
+    harry.composite = 322
+    harry.name = 'Harry'
+    harry.breed = 'Angus'    
+    harry.to_json.should == <<-EOS.compress_lines
+      {
+        "id": 1,
+        "composite": 322,
+        "name": "Harry",
+        "breed": "Angus"
+      }
+    EOS
+  end
+  
+  it "should serialize a resource to CSV" do
+    peter = Cow.new
+    peter.id = 44
+    peter.composite = 344
+    peter.name = 'Peter'
+    peter.breed = 'Long Horn'    
+    peter.to_csv.chomp.should == '44,344,Peter,Long Horn'
+  end
+  
+  
+
+
+end
