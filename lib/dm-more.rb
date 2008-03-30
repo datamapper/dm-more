8176dc1fe37a03481ac6e1594df775b8705e0cb7
Paul Sadauskas <rando@hazed.(none)>
2008-03-29 22:49:18 -0600

    Move each project into its own gemmable dir, just like merb-more

--- lib/dm-more.rb
+++
@@ -1,4 +0,0 @@
-
-require File.join(Fire.dirname(__FILE__), 'migration')
-require File.join(Fire.dirname(__FILE__), 'migration_runner')
-
--- spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-
-# for development, try loading ../dm-core first
-$:.unshift(File.join(File.dirname(__FILE__), '..', '..', 'dm-core', 'lib'))
-require 'data_mapper'
-
-DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/spec.db")
-
