87dd10f8fd2acefcfa50df43fa39a8a2d4bf101a
Ben Burkert <ben@benburkert.com>
2008-05-09 16:30:13 -0500

    Added EpochTime custom type to dm-types
    * EpochTime translates Time's, DateTime's, and Integer's to their epoch equivalent.

---
+++ dm-types/spec/epoch_time_spec.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+require 'pathname'
+gem 'dm-core', '=0.9.0'
+require 'data_mapper'
+
+require Pathname(__FILE__).dirname.parent.expand_path + 'lib/dm-types'
+
+include DataMapper::Types
+
+describe DataMapper::Types::EpochTime do
+  describe ".dump" do    
+    it "should accept Time objects" do
+      t = Time.now
+      
+      EpochTime.dump(t, :property).should == t.to_i
+    end
+
+    it "should accept DateTime objects" do
+      t = DateTime.now
+      
+      EpochTime.dump(t, :property).should == Time.parse(t.to_s).to_i
+    end
+
+    it "should accept Integer objects" do
+      t = Time.now.to_i
+      
+      EpochTime.dump(t, :property).should == t
+    end
+  end
+  
+  describe ".load" do
+    
+    it "should load null as nil" do
+      EpochTime.load(nil, :property).should == nil
+    end
+    
+    it "should load #{Time.now.to_i} as Time.at(#{Time.now.to_i})" do
+      t = Time.now.to_i
+      EpochTime.load(Time.now.to_i, :property).should == Time.at(t)
+    end
+  end
+end
