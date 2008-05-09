eebbfaff34defab97f41fdae2e7615d029c626de
Ben Burkert <ben@benburkert.com>
2008-05-09 18:39:18 -0500

    forgot to add require statement for EpochType

--- dm-types/lib/dm-types.rb
+++ dm-types/lib/dm-types.rb
@@ -4,5 +4,6 @@ require 'data_mapper'
 
 require File.join(File.dirname(__FILE__),'dm-types','csv')
 require File.join(File.dirname(__FILE__),'dm-types','enum')
+require File.join(File.dirname(__FILE__),'dm-types','epoch_time')
 require File.join(File.dirname(__FILE__),'dm-types','flag')
 require File.join(File.dirname(__FILE__),'dm-types','yaml')
