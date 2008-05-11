014d2cde0fb68ddbda7b151ddbb2611b2016b8ec
Mayo Jordanov <mayo@oyam.ca>
2008-05-11 00:46:10 -0700

    Write a URI type based on addressable

---
+++ dm-types/spec/uri_spec.rb
@@ -0,0 +1,43 @@
+require 'rubygems'
+require 'pathname'
+gem 'dm-core', '=0.9.0'
+require 'data_mapper'
+
+require Pathname(__FILE__).dirname.parent.expand_path + 'lib/dm-types'
+
+include DataMapper::Types
+
+describe DataMapper::Types::Uri do
+  before(:each) do
+    @uri_str = "http://example.com/path/to/resource/"
+    @uri = Addressable::URI.parse(@uri_str)
+  end
+  
+  describe "#dump" do
+    it "should return the url as string" do
+      Uri.dump(@uri, :property).should == @uri_str
+    end
+    
+    it "should return nil if string is nil" do
+      Uri.dump(nil, :property).should be_nil
+    end
+    
+    it "should return empty uri if string is empty" do
+      Uri.dump("", :property).should == ""
+    end
+  end
+  
+  describe "#load" do    
+    it "should return the url as Addressable" do
+      Uri.load(@uri_str, :property).should == @uri
+    end
+    
+    it "should return nil if given nil" do
+      Uri.load(nil, :property).should be_nil
+    end
+    
+    it "should return empty uri if given empty string" do
+      Uri.load("", :property).should == Addressable::URI.parse("")
+    end
+  end
+end
