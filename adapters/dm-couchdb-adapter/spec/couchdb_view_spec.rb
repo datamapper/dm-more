72a178414f4a159b5d33878eab44389e5071d5fd
Abdul-Rahman Advany <abdulrahman@advany.com>
2008-05-28 14:39:14 +0200

    couch db should only run when it is included as an adapter (currectly I can't get it running)

--- adapters/dm-couchdb-adapter/spec/couchdb_view_spec.rb
+++ adapters/dm-couchdb-adapter/spec/couchdb_view_spec.rb
@@ -1,44 +1,52 @@
 require 'pathname'
 require Pathname(__FILE__).dirname.parent.expand_path + 'lib/couchdb_adapter'
 
-describe "DataMapper::View" do
-  before(:all) do
-    class Zoo
-      include DataMapper::Resource
-      property :id, Fixnum, :serial => true
-      property :name, String
-      property :open, TrueClass
+if DataMapper::Repository.adapters.has_key?(:couchdb)
+
+  describe "DataMapper::View" do
+    before(:all) do
+      class Zoo
+        include DataMapper::Resource
+        property :id, Fixnum, :serial => true
+        property :name, String
+        property :open, TrueClass
+      end
+    end
+
+    it "should have a view method" do
+      Zoo.should respond_to(:view)
+    end
+
+    it "should store a view when called" do
+      Zoo.view :by_name
+      Zoo.views.keys.should include(:by_name)
+    end
+
+    it "should initialize a new Procedure instance" do
+      proc = Zoo.view :by_name_desc
+      proc.should be_an_instance_of(DataMapper::View)
+    end
+
+    it "should create a getter method" do
+      Zoo.view :open
+      Zoo.should respond_to(:open)
     end
   end
 
-  it "should have a view method" do
-    Zoo.should respond_to(:view)
+  describe "DataMapper::Repository" do
+    it "should define a view method" do
+      repository(:default).should respond_to(:view)
+    end
   end
 
-  it "should store a view when called" do
-    Zoo.view :by_name
-    Zoo.views.keys.should include(:by_name)
+  describe "DataMapper::Adapters::AbstractAdapter" do
+    it "should have a view method" do
+      DataMapper::Adapters::AbstractAdapter.instance_methods.should include("view")
+    end
   end
 
-  it "should initialize a new Procedure instance" do
-    proc = Zoo.view :by_name_desc
-    proc.should be_an_instance_of(DataMapper::View)
+else
+  describe DataMapper::Adapters::CouchdbAdapter do
+    it "No CouchDB found"
   end
-
-  it "should create a getter method" do
-    Zoo.view :open
-    Zoo.should respond_to(:open)
-  end
-end
-
-describe "DataMapper::Repository" do
-  it "should define a view method" do
-    repository(:default).should respond_to(:view)
-  end
-end
-
-describe "DataMapper::Adapters::AbstractAdapter" do
-  it "should have a view method" do
-    DataMapper::Adapters::AbstractAdapter.instance_methods.should include("view")
-  end
-end
+end # check couchdb
