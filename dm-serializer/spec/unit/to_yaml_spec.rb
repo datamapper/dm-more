5a4bcf9a6ce8f61533bfbc4d1dd18119a43096a5
Michael S. Klishin <michael@novemberain.com>
2008-05-29 02:13:48 +0300

    Use YAML deserialization in to_yaml spec.

--- dm-serializer/spec/unit/to_yaml_spec.rb
+++ dm-serializer/spec/unit/to_yaml_spec.rb
@@ -10,6 +10,8 @@ describe DataMapper::Serialize do
     @collection = DataMapper::Collection.new(DataMapper::repository(:default), Cow, properties_with_indexes)
     @collection.load([1, 2, 'Betsy', 'Jersey'])
     @collection.load([10, 20, 'Berta', 'Guernsey'])
+
+    @empty_collection = DataMapper::Collection.new(DataMapper::repository(:default), Cow, properties_with_indexes)
   end
 
   #
@@ -19,27 +21,43 @@ describe DataMapper::Serialize do
   describe "#to_yaml" do
     it "serializes single resource to YAML" do
       betsy = Cow.new(:id => 230, :composite => 22, :name => "Betsy", :breed => "Jersey")
-      betsy.to_yaml.gsub(/[[:space:]]+\n/, "\n").strip.should == <<-EOS.margin
-      ---
-      :id: 230
-      :composite: 22
-      :name: Betsy
-      :breed: Jersey
-    EOS
+      deserialized_hash = YAML.load(betsy.to_yaml)
+
+      deserialized_hash[:id].should        == 230
+      deserialized_hash[:name].should      == "Betsy"
+      deserialized_hash[:composite].should == 22
+      deserialized_hash[:breed].should     == "Jersey"
     end
 
+    it "leaves out nil properties" do
+      betsy = Cow.new(:id => 230, :name => "Betsy", :breed => "Jersey")
+      deserialized_hash = YAML.load(betsy.to_yaml)
+
+      deserialized_hash[:id].should        == 230
+      deserialized_hash[:name].should      == "Betsy"
+      deserialized_hash[:composite].should be(nil)
+      deserialized_hash[:breed].should     == "Jersey"
+    end    
+
     it "serializes a collection to YAML" do
-      @collection.to_yaml.gsub(/[[:space:]]+\n/, "\n").strip.should == <<-EOS.margin
-      ---
-      - :id: 1
-        :composite: 2
-        :name: Betsy
-        :breed: Jersey
-      - :id: 10
-        :composite: 20
-        :name: Berta
-        :breed: Guernsey
-    EOS
+      deserialized_collection = YAML.load(@collection.to_yaml)
+
+      betsy = deserialized_collection.first
+      berta = deserialized_collection.last
+
+      betsy[:id].should        == 1
+      betsy[:name].should      == "Betsy"
+      betsy[:composite].should == 2
+      betsy[:breed].should     == "Jersey"
+
+      berta[:id].should        == 10
+      berta[:name].should      == "Berta"
+      berta[:composite].should == 20
+      berta[:breed].should     == "Guernsey"
+    end
+
+    it "handles empty collections just fine" do
+      YAML.load(@empty_collection.to_yaml).should be_empty
     end
   end
 end
