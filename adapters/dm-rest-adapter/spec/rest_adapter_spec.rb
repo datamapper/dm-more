be015a6e356741f0bf3b43b77258b40123c608f4
Paul Barry <mail@paulbarry.com>
2008-06-08 17:41:15 -0400

    Updated spec to reflect the test app

--- adapters/dm-rest-adapter/spec/rest_adapter_spec.rb
+++ adapters/dm-rest-adapter/spec/rest_adapter_spec.rb
@@ -1,13 +1,27 @@
 require 'pathname'
 require Pathname(__FILE__).dirname.parent.expand_path + 'lib/rest_adapter'
 
-DataMapper.setup(:rest, {
+DataMapper.setup(:default, {
   :adapter  => 'rest',
   :format => 'xml',
   :base_url => 'http://whatever.com/api'
 })
 
+class Book
+  include DataMapper::Resource
+  property :id,         Integer, :serial => true
+  property :title,      String
+  property :author,     String
+  property :created_at, DateTime
+end
 
 describe "The REST Adapter" do
-  it "should have some specs"
+  it "should be able to create a book" do
+    pending
+    Book.create(:title => "Hello, World!", :author => "Anonymous")
+  end
+  it "should be able to get all the books" do
+    pending
+    Book.all.should_not be_empty
+  end
 end
