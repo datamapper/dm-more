26e0674cc43a1388424436d4b392b10fd67fd956
Nolan Darilek <nolan@thewordnerd.info>
2008-06-08 16:09:56 -0500

    Added JSON type.

---
+++ dm-types/lib/dm-types/json.rb
@@ -0,0 +1,31 @@
+require 'json'
+
+module DataMapper
+  module Types
+    class Json < DataMapper::Type
+      primitive String
+      size 65535
+      lazy true
+
+      def self.load(value, property)
+        if value.nil?
+          nil
+        elsif value.is_a?(String)
+          ::JSON.load(value)
+        else
+          raise ArgumentError.new("+value+ must be nil or a String")
+        end
+      end
+
+      def self.dump(value, property)
+        if value.nil?
+          nil
+        elsif value.is_a?(String)
+          value
+        else
+          ::JSON.dump(value)
+        end
+      end
+    end # class Yaml
+  end # module Types
+end # module DataMapper
