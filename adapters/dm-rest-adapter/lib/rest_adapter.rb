e017a281878692216233d9bf46cb2ff8276b6065
Paul Barry <mail@paulbarry.com>
2008-06-08 18:15:04 -0400

    Started work on implementation

--- adapters/dm-rest-adapter/lib/rest_adapter.rb
+++ adapters/dm-rest-adapter/lib/rest_adapter.rb
@@ -7,6 +7,11 @@ require 'net/http'
 module DataMapper
   module Adapters
     class RestAdapter < AbstractAdapter
+      # Creates a new resource in the specified repository.
+      def create(repository, resource)
+        result = http_post("/#{resource.storage_name}", resource.to_http_post_data)
+        #TODO: Check to see if result is HTTP 200, convert XML data to Array of instances of DM Resource 
+      end
     end
   end
 end
--- adapters/dm-rest-adapter/spec/rest_adapter_spec.rb
+++ adapters/dm-rest-adapter/spec/rest_adapter_spec.rb
@@ -15,13 +15,26 @@ class Book
   property :created_at, DateTime
 end
 
-describe "The REST Adapter" do
-  it "should be able to create a book" do
-    pending
-    Book.create(:title => "Hello, World!", :author => "Anonymous")
+describe Book do
+  it "should use 'books' for the storage name" do
+    Book.storage_name.should == "books"
   end
   it "should be able to get all the books" do
     pending
     Book.all.should_not be_empty
+  end
+end
+
+describe "A Book" do
+  before do
+    @book = Book.new(:title => "Hello, World!", :author => "Anonymous")
+  end
+  it "should be able to render itself as HTTP Post Data" do
+    pending "Need a method to convert a resource to HTTP Post Data"
+    @book.to_http_post_data.should == "book[name]=Hello%2C+World&book[author]=Anonymous"
+  end
+  it "should be able to create a book" do
+    pending
+    @book.save
   end
 end
