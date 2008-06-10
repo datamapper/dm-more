f18fd79f8a7e019351c8d159d5ab71018a4a9623
Sindre Aarsaether <sindre@identu.no>
2008-06-10 09:55:24 +0200

    added example / placeholder for Is-plugins

---
+++ dm-is-example/lib/dm-is-example.rb
@@ -0,0 +1,20 @@
+# Needed to import datamapper and other gems
+require 'rubygems'
+require 'pathname'
+
+# Add all external dependencies for the plugin here
+gem 'dm-core', '=0.9.0'
+require 'dm-core'
+
+# Require plugin-files
+require Pathname(__FILE__).dirname.expand_path / 'dm-is-example' / 'is' / 'example.rb'
+
+
+# Include the plugin in Resource
+module DataMapper
+  module Resource
+    module ClassMethods
+      include DataMapper::Is::Example
+    end # module ClassMethods
+  end # module Resource
+end # module DataMapper
