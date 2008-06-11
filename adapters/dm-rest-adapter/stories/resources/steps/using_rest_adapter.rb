5f6fd3617834f62ff7e82a108e390e0c9fe43cff
Evan David Light <evan@tiggerpalace.com>
2008-06-10 23:58:17 -0400

    Stories for CRUD

--- adapters/dm-rest-adapter/stories/resources/steps/using_rest_adapter.rb
+++ adapters/dm-rest-adapter/stories/resources/steps/using_rest_adapter.rb
@@ -22,13 +22,70 @@ steps_for :using_rest_adapter do
   Given("a valid DataMapper::Resource") do
     @resource = new_book
   end
+
+  Given("a DataMapper::Resource representing a type of Resource") do
+    @type = Book
+  end
+
+  Given("the ID of an existing Resource") do
+    @resource_id = 1
+  end
+
+  Given("the ID of a nonexistent Resource") do
+    @resource_id = 42 ** 42
+  end
+
+  Given("a local representation of a remote Resource") do
+    @resource = Book.get(1)
+  end
   
   When("I try to save the Resource") do
     @result = @resource.save
   end
   
-  Then("the result should indicate success") do
+  When("I request all of the Resources of that type") do
+    @resources = @type.all
+  end
+  
+  When("I request the Resource") do
+    @resource = @type.get(@resource_id)
+  end
+
+  When("I make valid changes to that Resource") do
+    @resource.title = "Mary had a little lamb"
+  end
+
+  When("I make invalid changes to that Resource") do
+    @resource.title = nil
+  end
+  
+  When("I destroy the Resource") do
+    @type.destroy(@resource_id)
+  end
+  
+  Then("the Resource should save") do
     @result.should be_true
+  end
+  
+  Then("the Resource should not save") do
+    @result.should be_false
+  end
+  
+  Then("I should not receive an empty list") do
+    @resources.should_not be_empty
+  end
+
+  Then("I should receive that Resource") do
+    @resource.should_not be_nil
+    @reourece.id.should == @resource_id
+  end
+
+  Then("I should get nothing in return") do
+    @resource.should be_nil
+  end
+  
+  Then("the Resource will no longer be available") do
+    @type.get(@resource_id).should be_nil
   end
 end
 
