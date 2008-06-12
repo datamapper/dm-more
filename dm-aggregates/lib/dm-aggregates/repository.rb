44c77838f13aee92b538152cae31741606a154b8
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-06-11 20:54:47 -0700

    Updated to use adapter accessor rather than ivar

--- dm-aggregates/lib/dm-aggregates/repository.rb
+++ dm-aggregates/lib/dm-aggregates/repository.rb
@@ -1,23 +1,23 @@
 module DataMapper
   class Repository
     def count(model, property, query)
-      @adapter.count(self, property, scoped_query(model, query))
+      adapter.count(self, property, scoped_query(model, query))
     end
 
     def min(model, property, query)
-      @adapter.min(self, property, scoped_query(model, query))
+      adapter.min(self, property, scoped_query(model, query))
     end
 
     def max(model, property, query)
-      @adapter.max(self, property, scoped_query(model, query))
+      adapter.max(self, property, scoped_query(model, query))
     end
 
     def avg(model, property, query)
-      @adapter.avg(self, property, scoped_query(model, query))
+      adapter.avg(self, property, scoped_query(model, query))
     end
 
     def sum(model, property, query)
-      @adapter.sum(self, property, scoped_query(model, query))
+      adapter.sum(self, property, scoped_query(model, query))
     end
 
     private
