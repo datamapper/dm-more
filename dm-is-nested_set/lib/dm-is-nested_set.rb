834c9bd3f0340c5a437e019a6632b3b49a065f50
Sindre Aarsaether <sindre@Sindre-Aarsaethers-MacPro.local>
2008-06-15 00:30:01 +0200

    fixed gem-version for dm-adjust

--- dm-is-nested_set/lib/dm-is-nested_set.rb
+++ dm-is-nested_set/lib/dm-is-nested_set.rb
@@ -4,6 +4,9 @@ require 'pathname'
 gem 'dm-core', '=0.9.2'
 require 'dm-core'
 
+gem 'dm-adjust', '=0.9.2'
+require 'dm-adjust'
+
 require Pathname(__FILE__).dirname.expand_path / 'dm-is-nested_set' / 'is' / 'nested_set.rb'
 
 module DataMapper
