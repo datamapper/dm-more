5bb5c7fa48c5499488c5c284c37264bf159ff220
Jonathan Stott <jonathan.stott@gmail.com>
2008-06-14 22:55:17 +0100

    Added rake tasks to merb_datamapper to use dm-migrations
    These tasks allow one to migrate up or down. Currently, it's just 'all the way
    up' and 'all the way down'.  The tasks appear to work, though I'm not yet sure
    how to spec them.

--- merb_datamapper/lib/merb_datamapper/merbtasks.rb
+++ merb_datamapper/lib/merb_datamapper/merbtasks.rb
@@ -16,6 +16,25 @@ namespace :dm do
     task :autoupgrade => :merb_start do
       ::DataMapper::AutoMigrator.auto_upgrade
     end
+
+    namespace :migrate do
+      task :load => :merb_start do
+        gem 'dm-migrations'
+        require 'migration_runner'
+        FileList["db/migrations/*.rb"].each do |migration|
+          load migration
+        end
+      end
+
+      desc "Migrate up using migrations"
+      task :up => :load do
+        migrate_up!
+      end
+      desc "Migrate down using migrations"
+      task :down => :load do
+        migrate_down!
+      end
+    end
   end
 
   namespace :sessions do
