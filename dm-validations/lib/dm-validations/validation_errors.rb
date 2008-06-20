97dcd1b2fb1061f93ff59922b4ef3c32e1090c9a
Sam Smoot <ssmoot@gmail.com>
2008-06-19 20:38:34 -0500

    Lazily instantiate the internal Hash for ValidationErrors, some implementation tweaks to yield only non-empty errors, made ValidationErrors an Enumerable.

--- dm-validations/lib/dm-validations/validation_errors.rb
+++ dm-validations/lib/dm-validations/validation_errors.rb
@@ -7,13 +7,11 @@ module DataMapper
     # @since  0.9
     class ValidationErrors
 
-      def initialize
-        @errors = {}
-      end
-
+      include Enumerable
+      
       # Clear existing validation errors.
       def clear!
-        @errors.clear
+        errors.clear
       end
 
       # Add a validation error. Use the field_name :general if the errors does
@@ -22,13 +20,12 @@ module DataMapper
       # @param <Symbol> field_name the name of the field that caused the error
       # @param <String> message    the message to add
       def add(field_name, message)
-        @errors[field_name] ||= []
-        @errors[field_name] << message
+        (errors[field_name] ||= []) << message
       end
 
       # Collect all errors into a single list.
       def full_messages
-        @errors.inject([]) do |list,pair|
+        errors.inject([]) do |list,pair|
           list += pair.last
         end
       end
@@ -37,17 +34,28 @@ module DataMapper
       #
       # @param <Symbol> field_name the name of the field you want an error for
       def on(field_name)
-        @errors[field_name].nil? ? nil : @errors[field_name]
+        errors_for_field = errors[field_name]
+        errors_for_field.blank? ? nil : errors_for_field
       end
 
       def each
-        @errors.map.each do |k,v|
+        errors.map.each do |k,v|
+          next if v.blank?
           yield(v)
         end
       end
 
+      def empty?
+        entries.empty?
+      end
+      
       def method_missing(meth, *args, &block)
-        @errors.send(meth, *args, &block)
+        errors.send(meth, *args, &block)
+      end
+      
+      private
+      def errors
+        @errors ||= {}
       end
 
     end # class ValidationErrors
