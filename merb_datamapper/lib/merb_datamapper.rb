b34501dd445ed13040ab50ae381bc0d49fb7d8d8
Ben Burkert <ben@benburkert.com>
2008-06-29 21:58:02 -0500

    database connections are assumed to be in the environmental file if the database.yml file is not present.
    * Placed condition on "Merb::Orms::DataMapper.connect" and "Merb::Orms::DataMapper.register_session_type" calls.
    * Added logger message warning when the database connection should be established in the environmental file.

--- merb_datamapper/lib/merb_datamapper.rb
+++ merb_datamapper/lib/merb_datamapper.rb
@@ -1,10 +1,14 @@
-
-require File.join(File.dirname(__FILE__) / "merb" / "orms" / "data_mapper" / "connection")
-require File.join(File.dirname(__FILE__) / "merb" / "orms" / "data_mapper" / "resource")
-
-Merb::Orms::DataMapper.connect
-Merb::Orms::DataMapper.register_session_type
-
 if defined?(Merb::Plugins)
+  if File.file?(Merb.root / "config" / "database.yml")
+    require File.join(File.dirname(__FILE__) / "merb" / "orms" / "data_mapper" / "connection")
+
+    Merb::Orms::DataMapper.connect
+    Merb::Orms::DataMapper.register_session_type
+  else
+    Merb.logger.info "No database.yml file found in #{Merb.root}/config, assuming database connection(s) established in the environment file in #{Merb.root}/config/environments"
+  end
+
+  require File.join(File.dirname(__FILE__) / "merb" / "orms" / "data_mapper" / "resource")
+
   Merb::Plugins.add_rakefiles "merb_datamapper" / "merbtasks"
 end
