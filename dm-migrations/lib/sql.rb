0561bbe0f6c3243c871aca4085a4bb2ce2bf7bbc
Paul Sadauskas <pjs@absolute-performance.com>
2008-07-06 17:03:51 -0600

    Switch load order of sql extensions

--- dm-migrations/lib/sql.rb
+++ dm-migrations/lib/sql.rb
@@ -1,9 +1,9 @@
+require File.dirname(__FILE__) + '/sql/table_creator'
+require File.dirname(__FILE__) + '/sql/table_modifier'
+
 require File.dirname(__FILE__) + '/sql/sqlite3'
 require File.dirname(__FILE__) + '/sql/mysql'
 require File.dirname(__FILE__) + '/sql/postgresql'
-
-require File.dirname(__FILE__) + '/sql/table_creator'
-require File.dirname(__FILE__) + '/sql/table_modifier'
 
 module SQL
 
