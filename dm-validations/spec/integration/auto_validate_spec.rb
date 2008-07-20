3888768bcec19500fa17c45451a668063473db60
Adam French <adam@wieck.com>
2008-07-20 17:29:31 -0500

    including patch by teamon for [#472 state:resolved]

--- dm-validations/spec/integration/auto_validate_spec.rb
+++ dm-validations/spec/integration/auto_validate_spec.rb
@@ -259,4 +259,53 @@ describe "Automatic Validation from Property Definition" do
       end
     end
   end
+
+  describe 'for custom messages' do
+    it "should have correct error message" do
+      custom_boat = Class.new do
+        include DataMapper::Resource
+        property :id,   Integer, :serial => true
+        property :name, String,  :nullable => false, :message => "This boat must have name"
+      end
+      boat = custom_boat.new
+      boat.should_not be_valid
+
+      boat.errors.on(:name).should eql(["This boat must have name"])
+    end
+
+    it "should have correct error messages" do
+      custom_boat = Class.new do
+        include DataMapper::Resource
+        property :id,   Integer, :serial => true
+        property :name, String,  :nullable => false, :length => 5..20, :format => /^[a-z]+$/,
+                 :messages => {
+                   :presence => "This boat must have name",
+                   :length => "Name must have at least 4 and at most 20 chars",
+                   :format => "Please use only small letters"
+                 }
+      end
+
+      boat = custom_boat.new
+      boat.should_not be_valid
+      boat.errors.on(:name).should include("This boat must have name")
+      boat.errors.on(:name).should include("Name must have at least 4 and at most 20 chars")
+      boat.errors.on(:name).should include("Please use only small letters")
+
+      boat.name = "%%"
+      boat.should_not be_valid
+      boat.errors.on(:name).should_not include("This boat must have name")
+      boat.errors.on(:name).should include("Name must have at least 4 and at most 20 chars")
+      boat.errors.on(:name).should include("Please use only small letters")
+
+      boat.name = "%%asd"
+      boat.should_not be_valid
+      boat.errors.on(:name).should_not include("This boat must have name")
+      boat.errors.on(:name).should_not include("Name must have at least 4 and at most 20 chars")
+      boat.errors.on(:name).should include("Please use only small letters")
+
+      boat.name = "superboat"
+      boat.should be_valid
+      boat.errors.on(:name).should be_nil
+    end
+  end
 end
