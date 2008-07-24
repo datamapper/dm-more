d4fcc7de86a19477b13620a970e9d9453508596d
Bernerd Schaefer <bj.schaefer@gmail.com>
2008-07-24 16:42:38 -0500

    Added == method to generic validator to prevent exact duplicate validations from getting added.

--- dm-validations/lib/dm-validations/generic_validator.rb
+++ dm-validations/lib/dm-validations/generic_validator.rb
@@ -87,6 +87,13 @@ module DataMapper
         return true
       end
 
+      def ==(other)
+        self.field_name == other.field_name &&
+        self.if_clause == other.if_clause &&
+        self.unless_clause == other.unless_clause &&
+        self.instance_variable_get(:@options) == other.instance_variable_get(:@options)
+      end
+
     end # class GenericValidator
   end # module Validate
 end # module DataMapper
