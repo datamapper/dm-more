c3ff248076ca7a2ec26db4e243706057f0cec2da
Rob Westgeest <rob.westgeest@gmail.com>
2008-08-24 22:07:50 +0200

    enabled multiple validations on one field if they are of a different kind

--- dm-validations/lib/dm-validations/generic_validator.rb
+++ dm-validations/lib/dm-validations/generic_validator.rb
@@ -88,6 +88,7 @@ module DataMapper
 
       def ==(other)
         self.field_name == other.field_name &&
+        self.class == other.class &&
         self.if_clause == other.if_clause &&
         self.unless_clause == other.unless_clause &&
         self.instance_variable_get(:@options) == other.instance_variable_get(:@options)
