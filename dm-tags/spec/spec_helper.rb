e6fdd4f96b8a1e921a52370308c3b1bcb2de7454
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-12-01 20:56:18 -0800

    [dm-tags] Minor update to spec_helper.rb

--- dm-tags/spec/spec_helper.rb
+++ dm-tags/spec/spec_helper.rb
@@ -1,9 +1,15 @@
 require 'rubygems'
-require 'spec'
+require 'pathname'
+
+gem 'dm-core', '~>0.9.7'
 require 'dm-core'
+
+gem 'dm-validations', '~>0.9.7'
 require 'dm-validations'
-require File.dirname(__FILE__) + '/../lib/dm-tags'
-require File.dirname(__FILE__) + '/classes'
+
+spec_dir_path = Pathname(__FILE__).dirname.expand_path
+require spec_dir_path.parent + 'lib/dm-tags'
+require spec_dir_path + 'classes'
 
 DataMapper.setup(:default, 'sqlite3::memory:')
 DataMapper.auto_migrate!
