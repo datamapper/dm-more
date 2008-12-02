a80fac40aa4de205c4637b0308616c85b3918cc4
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-12-01 21:01:52 -0800

    [dm-tags] Merged spec/classes.rb into spec_helper

--- dm-tags/spec/spec_helper.rb
+++ dm-tags/spec/spec_helper.rb
@@ -9,7 +9,32 @@ require 'dm-validations'
 
 spec_dir_path = Pathname(__FILE__).dirname.expand_path
 require spec_dir_path.parent + 'lib/dm-tags'
-require spec_dir_path + 'classes'
+
+class TaggedModel
+  include DataMapper::Resource
+  property :id, Serial
+
+  has_tags_on :skills, :interests, :tags
+end
+
+class AnotherTaggedModel
+  include DataMapper::Resource
+  property :id, Serial
+
+  has_tags_on :skills, :pets
+end
+
+class DefaultTaggedModel
+  include DataMapper::Resource
+  property :id, Serial
+
+  has_tags
+end
+
+class UntaggedModel
+  include DataMapper::Resource
+  property :id, Serial
+end
 
 DataMapper.setup(:default, 'sqlite3::memory:')
 DataMapper.auto_migrate!
