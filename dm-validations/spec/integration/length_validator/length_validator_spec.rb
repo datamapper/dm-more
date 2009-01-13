387f1d1d003fff8e77aad4548f2ed2811cddff97
Michael S. Klishin <michael@novemberain.com>
2009-01-13 14:31:12 +0300

    add a per-validator spec_helper, to length validator first

--- dm-validations/spec/integration/length_validator/length_validator_spec.rb
+++ dm-validations/spec/integration/length_validator/length_validator_spec.rb
@@ -1,18 +1,9 @@
 require 'pathname'
-require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
+__dir__ = Pathname(__FILE__).dirname.expand_path
 
-class MotorLaunch
-  include DataMapper::Resource
-  property :id, Integer, :serial => true
-  property :name, String, :auto_validation => false
-end
-
-class BoatDock
-  include DataMapper::Resource
-  property :id, Integer, :serial => true
-  property :name, String, :auto_validation => false, :default => "I'm a long string"
-  validates_length :name, :min => 3
-end
+# global first, then local to length validators
+require __dir__.parent.parent + "spec_helper"
+require __dir__ + 'spec_helper'
 
 describe DataMapper::Validate::LengthValidator do
   it "lets user specify custom error message" do
