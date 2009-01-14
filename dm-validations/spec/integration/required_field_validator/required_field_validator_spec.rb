becfe8f0899f7c117de22b3c237dc97df455c74d
Michael S. Klishin <michael@novemberain.com>
2009-01-14 15:05:20 +0300

    [dm-validations] more spec examples for validates_present

--- dm-validations/spec/integration/required_field_validator/required_field_validator_spec.rb
+++ dm-validations/spec/integration/required_field_validator/required_field_validator_spec.rb
@@ -63,6 +63,70 @@ if HAS_SQLITE3 || HAS_MYSQL || HAS_POSTGRES
       end
     end # describe "without explicitly specified committer name"
 
+    describe "WITH explicitly specified committer name" do
+      before :each do
+        @operation.committer_name = "Core Team Guy"
+      end
+
+      it "is valid for committing" do
+        @operation.should be_valid_for_committing
+      end
+
+      it "is not valid in default context" do
+        # context here is :default
+        @operation.should_not be_valid
+      end
+
+      it "has value set" do
+        # this is more of a sanity check since
+        # this sort of functionality clearly needs to be
+        # tested in
+        @operation.committer_name.should == "Core Team Guy"
+      end
+    end # describe "with explicitly specified committer name"
+
+
+
+    describe "without explicitly specified author name" do
+      before :each do
+        # no specific actions for this case! yay!
+      end
+
+      it "is valid for committing (because default value jumps in)" do
+        @operation.should be_valid_for_committing
+      end
+
+      it "is not valid in default context" do
+        # context here is :default
+        @operation.should_not be_valid
+      end
+
+      it "has default value set" do
+        @operation.author_name.should == "Just another Ruby hacker"
+      end
+    end # describe "without explicitly specified author name"
+
+    describe "WITH explicitly specified author name" do
+      before :each do
+        @operation.author_name = "Random contributor"
+      end
+
+      it "is valid for committing" do
+        @operation.should be_valid_for_committing
+      end
+
+      it "is not valid in default context" do
+        # context here is :default
+        @operation.should_not be_valid
+      end
+
+      it "has value set" do
+        @operation.author_name.should == "Random contributor"
+      end
+    end # describe "with explicitly specified author name"
+
+
+
     describe "without network connection" do
       before(:each) do
         # now note that false make sense from readability
@@ -91,6 +155,51 @@ if HAS_SQLITE3 || HAS_MYSQL || HAS_POSTGRES
     describe "with a network connection" do
       before(:each) do
         @operation.network_connection = false
+      end
+
+      it "is valid for committing" do
+        @operation.should be_valid_for_committing
+      end
+
+      it "is valid for pushing" do
+        @operation.should be_valid_for_pushing
+      end
+
+      it "is valid for pulling" do
+        @operation.should be_valid_for_pulling
+      end
+
+      it "is not valid in default context" do
+        @operation.should_not be_valid
+      end
+    end # describe "with a network connection"
+
+
+    describe "WITHOUT a clean working copy" do
+      before(:each) do
+        @operation.clean_working_copy = nil
+      end
+
+      it "is valid for committing" do
+        @operation.should be_valid_for_committing
+      end
+
+      it "is valid for pushing" do
+        @operation.should be_valid_for_pushing
+      end
+
+      it "is not valid for pulling" do
+        @operation.should_not be_valid_for_pulling
+      end
+
+      it "is not valid in default context" do
+        @operation.should_not be_valid
+      end
+    end # describe "without network connection"
+
+    describe "with a clean working copy" do
+      before(:each) do
+        @operation.clean_working_copy = true
       end
 
       it "is valid for committing" do
