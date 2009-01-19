09de831b5277620adc300a4cbea236b8b6ccd34b
Wesley Beary <monki@geemus.com>
2009-01-19 09:30:37 -0800

    change %2F to / in auto_migrate

--- adapters/dm-couchdb-adapter/lib/couchdb_adapter.rb
+++ adapters/dm-couchdb-adapter/lib/couchdb_adapter.rb
@@ -378,7 +378,7 @@ module DataMapper
 
       module Migration
         def create_model_storage(repository, model)
-          uri = "/#{self.escaped_db_name}/_design%2F#{model.base_model.to_s}"
+          uri = "/#{self.escaped_db_name}/_design/#{model.base_model.to_s}"
           view = Net::HTTP::Put.new(uri)
           view['content-type'] = "application/json"
           views = model.views.reject {|key, value| value.nil?}
@@ -389,7 +389,7 @@ module DataMapper
         end
 
         def destroy_model_storage(repository, model)
-          uri = "/#{self.escaped_db_name}/_design%2F#{model.base_model.to_s}"
+          uri = "/#{self.escaped_db_name}/_design/#{model.base_model.to_s}"
           response = http_get(uri)
           unless response['error']
             uri += "?rev=#{response["_rev"]}"
