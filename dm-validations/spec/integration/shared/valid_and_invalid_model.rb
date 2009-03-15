c22e3737c1f55f27e99af727879464408626b3d7
Michael S. Klishin <michael@novemberain.com>
2009-03-15 20:14:51 +0300

    [dm-validations] Add error message expectations to 'valid model' and 'invalid model' shared spec groups

--- dm-validations/spec/integration/shared/valid_and_invalid_model.rb
+++ dm-validations/spec/integration/shared/valid_and_invalid_model.rb
@@ -6,10 +6,22 @@ describe "valid model", :shared => true do
   it "has no error messages" do
     @model.errors.should be_empty
   end
+
+  it "has empty list of full error messages" do
+    @model.errors.full_messages.should be_empty
+  end
 end
 
 describe "invalid model", :shared => true do
   it "is NOT valid" do
     @model.should_not be_valid
+  end
+
+  it "has error messages" do
+    @model.errors.should_not be_blank
+  end
+
+  it "has list of full error messages" do
+    @model.errors.full_messages.should_not be_blank
   end
 end
