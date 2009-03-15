dbad5e351d8bb4ed502f0a2dc3e89153a50f029f
Michael S. Klishin <michael@novemberain.com>
2009-03-15 18:25:58 +0300

    [dm-validations] New spec examples for GenericValidator#==

---
+++ dm-validations/spec/unit/generic_validator/equality_operator_spec.rb
@@ -0,0 +1,27 @@
+require 'pathname'
+require Pathname(__FILE__).dirname.expand_path.parent + '../spec_helper'
+
+describe DataMapper::Validate::GenericValidator do
+  describe "when types and fields are equal" do
+    it "returns true" do
+      DataMapper::Validate::RequiredFieldValidator.new(:name).
+        should == DataMapper::Validate::RequiredFieldValidator.new(:name)
+    end
+  end # describe "#==" do
+
+
+  describe "when types differ" do
+    it "returns false" do
+      DataMapper::Validate::RequiredFieldValidator.new(:name).
+        should_not == DataMapper::Validate::UniquenessValidator.new(:name)
+    end
+  end
+
+
+  describe "when property names differ" do
+    it "returns false" do
+      DataMapper::Validate::RequiredFieldValidator.new(:first_name).
+        should_not == DataMapper::Validate::RequiredFieldValidator.new(:last_name)
+    end
+  end
+end # describe DataMapper::Validate::GenericValidator
