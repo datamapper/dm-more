7358a62499a5d53d6fe4f2d7c53a3cca653bd249
Michael S. Klishin <michael@novemberain.com>
2009-03-15 17:36:33 +0300

    [dm-validations] New specs for PrimitiveValidator

---
+++ dm-validations/spec/integration/primitive_validator/primitive_validator_spec.rb
@@ -0,0 +1,94 @@
+require 'pathname'
+
+__dir__ = Pathname(__FILE__).dirname.expand_path
+require __dir__.parent.parent + 'spec_helper'
+require __dir__ + 'spec_helper'
+
+
+describe DataMapper::Validate::Fixtures::MemoryObject do
+  include DataMapper::Validate::Fixtures
+
+  before :all do
+    @model = DataMapper::Validate::Fixtures::MemoryObject.new
+  end
+
+  describe "with color given as a string" do
+    before :all do
+      @model.color = "grey"
+    end
+
+    it "is valid" do
+      @model.should be_valid
+    end
+  end
+
+
+  describe "with color given as an object" do
+    before :all do
+      # we have to go through the back door
+      # since writer= method does typecasting
+      # and Object is casted to String
+      @model.instance_variable_set(:@color,  Object.new)
+    end
+
+    it "is NOT valid" do
+      @model.should_not be_valid
+    end
+  end
+
+
+  describe "with mark flag set to true" do
+    before :all do
+      @model.marked = true
+    end
+
+    it "is valid" do
+      @model.should be_valid
+    end
+  end
+
+
+  describe "with mark flag set to false" do
+    before :all do
+      @model.marked = false
+    end
+
+    it "is valid" do
+      @model.should be_valid
+    end
+  end
+
+  describe "with mark flag set to an object" do
+    before :all do
+      # go through the back door to avoid typecasting
+      @model.instance_variable_set(:@marked, Object.new)
+    end
+
+    it "is NOT valid" do
+      @model.should_not be_valid
+    end
+  end
+
+
+  describe "with color set to nil" do
+    before :all do
+      # go through the back door to avoid typecasting
+      @model.color = nil
+    end
+
+    it "is valid" do
+      @model.should be_valid
+    end
+  end
+
+
+  describe "with mark flag set to nil" do
+    before :all do
+      @model.marked = nil
+    end
+
+    it "is valid" do
+      @model.should be_valid
+    end
+  end
+end
---
+++ dm-validations/spec/integration/primitive_validator/spec_helper.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+module DataMapper
+  module Validate
+    module Fixtures
+      class MemoryObject
+        #
+        # Behaviors
+        #
+
+        include ::DataMapper::Resource
+        
+        #
+        # Properties
+        #
+
+        property :id,     Serial
+        property :marked, Boolean, :auto_validation => false
+        property :color,  String,  :auto_validation => false
+
+        #
+        # Validations
+        #
+
+        validates_is_primitive :marked
+        validates_is_primitive :color
+      end
+    end
+  end
+end
