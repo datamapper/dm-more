33ace90ebf8786245f5957846632e72a35641cdf
Michael S. Klishin <michael@novemberain.com>
2009-03-22 03:43:34 +0300

    [dm-types] Reorganize EpochTime type specs

--- dm-types/spec/unit/epoch_time_spec.rb
+++ dm-types/spec/unit/epoch_time_spec.rb
@@ -4,34 +4,59 @@ require Pathname(__FILE__).dirname.parent.expand_path + 'spec_helper'
 describe DataMapper::Types::EpochTime do
 
   describe ".dump" do
-    it "should accept Time objects" do
-      t = Time.now
+    describe "when given Time instance" do
+      before :all do
+        @input = Time.now
+      end
 
-      DataMapper::Types::EpochTime.dump(t, :property).should == t.to_i
+      it "returns timestamp" do
+        DataMapper::Types::EpochTime.dump(@input, :property).should == @input.to_i
+      end
     end
 
-    it "should accept DateTime objects" do
-      t = DateTime.now
+    describe "when given DateTime instance" do
+      before :all do
+        @input = DateTime.now
+      end
 
-      DataMapper::Types::EpochTime.dump(t, :property).should == Time.parse(t.to_s).to_i
+      it "returns timestamp" do
+        DataMapper::Types::EpochTime.dump(@input, :property).should == Time.parse(@input.to_s).to_i
+      end
     end
 
-    it "should accept Integer objects" do
-      t = Time.now.to_i
+    describe "when given an integer" do
+      before :all do
+        @input = Time.now.to_i
+      end
 
-      DataMapper::Types::EpochTime.dump(t, :property).should == t
+      it "returns value as is" do
+        DataMapper::Types::EpochTime.dump(@input, :property).should == @input
+      end
+    end
+
+    describe "when given nil" do
+      before :all do
+        @input = nil
+      end
+
+      it "returns value as is" do
+        DataMapper::Types::EpochTime.dump(@input, :property).should == @input
+      end
     end
   end
 
   describe ".load" do
-
-    it "should load null as nil" do
-      DataMapper::Types::EpochTime.load(nil, :property).should == nil
+    describe "when value is nil" do
+      it "returns nil" do
+        DataMapper::Types::EpochTime.load(nil, :property).should == nil
+      end
     end
 
-    it "should load #{Time.now.to_i} as Time.at(#{Time.now.to_i})" do
-      t = Time.now.to_i
-      DataMapper::Types::EpochTime.load(Time.now.to_i, :property).should == Time.at(t)
+    describe "when value is an integer" do
+      it "returns time object from timestamp" do
+        t = Time.now.to_i
+        DataMapper::Types::EpochTime.load(Time.now.to_i, :property).should == Time.at(t)
+      end
     end
   end
 end
