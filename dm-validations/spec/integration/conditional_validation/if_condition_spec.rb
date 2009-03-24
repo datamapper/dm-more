3689d2205af8d15a46d3873998e35e934fb98a70
Michael S. Klishin <michael@novemberain.com>
2009-03-24 22:55:32 +0300

    [dm-validations] Spec examples for conditional validation with :if option

---
+++ dm-validations/spec/integration/conditional_validation/if_condition_spec.rb
@@ -0,0 +1,65 @@
+require 'pathname'
+__dir__ = Pathname(__FILE__).dirname.expand_path
+
+# global first, then local to length validators
+require __dir__.parent.parent + "spec_helper"
+require __dir__ + 'spec_helper'
+
+describe DataMapper::Validate::Fixtures::UDPPacket do
+  before :all do
+    @model = DataMapper::Validate::Fixtures::UDPPacket.new
+  end
+
+  describe "that is transported encapsulated into IPv4 packet" do
+    before :all do
+      @model.underlying_ip_version = 4
+    end
+
+    describe "and has no checksum" do
+      before :all do
+        @model.checksum = nil
+      end
+
+      it_should_behave_like "valid model"
+    end
+
+    describe "and has no checksum algorithm" do
+      before :all do
+        @model.checksum_algorithm = nil
+      end
+
+      it_should_behave_like "valid model"
+    end    
+  end
+
+
+  describe "that is transported encapsulated into IPv6 packet" do
+    before :all do
+      @model.underlying_ip_version = 6
+    end
+
+    describe "and has no checksum" do
+      before :all do
+        @model.checksum = nil
+      end
+
+      it_should_behave_like "invalid model"
+
+      it "has a meaningful error message" do
+        @model.errors.on(:checksum).should include("Checksum is mandatory when used with IPv6")
+      end
+    end
+
+    describe "and has no checksum algorithm" do
+      before :all do
+        @model.checksum_algorithm = nil
+      end
+
+      it_should_behave_like "invalid model"
+
+      it "has a meaningful error message" do
+        @model.errors.on(:checksum_algorithm).should include("Checksum algorithm is mandatory when used with IPv6")
+      end
+    end    
+  end  
+end
