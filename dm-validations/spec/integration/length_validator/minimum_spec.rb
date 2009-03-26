235e90d33f81988ecc2e76c829a9a8021cf9f835
Michael S. Klishin <michael@novemberain.com>
2009-03-27 01:12:40 +0300

    [dm-validations] Reworked spec examples for length validator with :min option
     * Also hook in one more :message option that uses callable object...
     * ...this is integration spec suite, after all

--- dm-validations/spec/integration/length_validator/minimum_spec.rb
+++ dm-validations/spec/integration/length_validator/minimum_spec.rb
@@ -5,30 +5,40 @@ __dir__ = Pathname(__FILE__).dirname.expand_path
 require __dir__.parent.parent + "spec_helper"
 require __dir__ + 'spec_helper'
 
-# FIXME: these oldish spec examples are messy and need to be organize
-# like the rest of new spec suite
+require 'pathname'
+__dir__ = Pathname(__FILE__).dirname.expand_path
 
-describe DataMapper::Validate::LengthValidator do
-  it "lets user specify a minimum length of a string field" do
-    class ::DataMapper::Validate::Fixtures::MotorLaunch
-      validates_length :name, :min => 3
-    end
+# global first, then local to length validators
+require __dir__.parent.parent + "spec_helper"
+require __dir__ + 'spec_helper'
 
-    launch = DataMapper::Validate::Fixtures::MotorLaunch.new
-    launch.name = 'Ab'
-    launch.should_not be_valid
-    launch.errors.on(:name).should include('Name must be more than 3 characters long')
-  end
-
-  it "aliases :minimum for :min" do
+describe ::DataMapper::Validate::Fixtures::MotorLaunch, "with name length required to be 5 chars minimum" do
+  before :all do
     class ::DataMapper::Validate::Fixtures::MotorLaunch
       validators.clear!
-      validates_length :name, :minimum => 3
+      validates_length :name, :min => 5, :message => Proc.new { "Name must be longer than 5 characters long" }
     end
 
-    launch = DataMapper::Validate::Fixtures::MotorLaunch.new
-    launch.name = 'Ab'
-    launch.should_not be_valid
-    launch.errors.on(:name).should include('Name must be more than 3 characters long')
+    @model = DataMapper::Validate::Fixtures::MotorLaunch.new
+  end
+
+  describe "and name that is longer than 5 characters" do
+    before :all do
+      @model.name = "DataMapper"
+    end
+
+    it_should_behave_like "valid model"
+  end
+
+  describe "and name that is shorter than 5 characters" do
+    before :all do
+      @model.name = "Ruby"
+    end
+
+    it_should_behave_like "invalid model"
+
+    it "has a meaningful error message on invalid property" do
+      @model.errors.on(:name).should include("Name must be longer than 5 characters long")
+    end
   end
 end
