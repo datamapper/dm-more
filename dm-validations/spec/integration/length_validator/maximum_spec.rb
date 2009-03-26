6b90a42dcb1c7078f1832cedc1a58d7224ab008d
Michael S. Klishin <michael@novemberain.com>
2009-03-27 01:09:26 +0300

    [dm-validations] Reworked spec examples for length validator with :max option

--- dm-validations/spec/integration/length_validator/maximum_spec.rb
+++ dm-validations/spec/integration/length_validator/maximum_spec.rb
@@ -5,29 +5,33 @@ __dir__ = Pathname(__FILE__).dirname.expand_path
 require __dir__.parent.parent + "spec_helper"
 require __dir__ + 'spec_helper'
 
-# FIXME: these oldish spec examples are messy and need to be organize
-# like the rest of new spec suite
-describe DataMapper::Validate::LengthValidator do
-  it "lets user specify a maximum length of a string field" do
+describe ::DataMapper::Validate::Fixtures::MotorLaunch, "with name length required to be 5 chars max" do
+  before :all do
     class ::DataMapper::Validate::Fixtures::MotorLaunch
       validators.clear!
       validates_length :name, :max => 5
     end
 
-    launch = DataMapper::Validate::Fixtures::MotorLaunch.new
-    launch.name = 'a' * 6
-    launch.should_not be_valid
-    launch.errors.on(:name).should include('Name must be less than 5 characters long')
+    @model = DataMapper::Validate::Fixtures::MotorLaunch.new
   end
 
-  it "aliases :maximum for :max" do
-    class ::DataMapper::Validate::Fixtures::MotorLaunch
-      validators.clear!
-      validates_length :name, :maximum => 5
+  describe "and name that is longer than 5 characters" do
+    before :all do
+      @model.name = "DataMapper"
     end
-    launch = DataMapper::Validate::Fixtures::MotorLaunch.new
-    launch.name = 'a' * 6
-    launch.should_not be_valid
-    launch.errors.on(:name).should include('Name must be less than 5 characters long')
+
+    it_should_behave_like "invalid model"
+
+    it "has a meaningful error message on invalid property" do
+      @model.errors.on(:name).should include('Name must be less than 5 characters long')
+    end
+  end
+
+  describe "and name that is shorter than 5 characters" do
+    before :all do
+      @model.name = "Ruby"
+    end
+
+    it_should_behave_like "valid model"
   end
 end
