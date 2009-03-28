79dd24807052675fda38c4ef192955ece4d5f0e7
Michael S. Klishin <michael@novemberain.com>
2009-03-28 15:21:05 +0300

    [dm-validations] Redundant spec examples for :eq/:equal option of numeric validator

--- dm-validations/spec/integration/numeric_validator/equality_with_float_type_spec.rb
+++ dm-validations/spec/integration/numeric_validator/equality_with_float_type_spec.rb
@@ -16,6 +16,16 @@ describe DataMapper::Validate::Fixtures::BasketballCourt do
   end
 
 
+  describe "with rim height of 3.05" do
+    before :all do
+      @model = DataMapper::Validate::Fixtures::BasketballCourt.valid_instance(:rim_height => 3.05)
+      @model.valid?
+    end
+
+    it_should_behave_like "valid model"
+  end
+
+
   describe "with rim height of 3.30" do
     before :all do
       @model = DataMapper::Validate::Fixtures::BasketballCourt.valid_instance(:rim_height => 3.30)
@@ -26,6 +36,30 @@ describe DataMapper::Validate::Fixtures::BasketballCourt do
 
     it "has a meaningful error message" do
       @model.errors.on(:rim_height).should include("Rim height must be a number equal to 3.05")
+    end
+  end
+
+
+  describe "with free throw line distance of 4.57" do
+    before :all do
+      @model = DataMapper::Validate::Fixtures::BasketballCourt.valid_instance(:free_throw_line_distance => 4.57)
+      @model.valid?
+    end
+
+    it_should_behave_like "valid model"
+  end
+
+
+  describe "with free throw line distance of 3.10" do
+    before :all do
+      @model = DataMapper::Validate::Fixtures::BasketballCourt.valid_instance(:free_throw_line_distance => 3.10)
+      @model.valid?
+    end
+
+    it_should_behave_like "invalid model"
+
+    it "has a meaningful error message" do
+      @model.errors.on(:free_throw_line_distance).should include("Free throw line distance must be a number equal to 4.57")
     end
   end
 end
