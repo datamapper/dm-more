efe20fcf8f3e9aba27388d4f5864519c2685c362
Michael S. Klishin <michael@novemberain.com>
2009-03-29 18:32:12 +0400

    [dm-validations] Split automatic validations generation into multiple shorter methods

--- dm-validations/lib/dm-validations/auto_validate.rb
+++ dm-validations/lib/dm-validations/auto_validate.rb
@@ -90,68 +90,12 @@ module DataMapper
         opts = { :allow_nil => property.nullable? || property.serial? }
         opts[:context] = property.options[:validates] if property.options.has_key?(:validates)
 
-        # presence
-        unless opts[:allow_nil]
-          # validates_present property.name, opts
-          validates_present property.name, options_with_message(opts, property, :presence)
-        end
-
-        # length
-        if [String, DataMapper::Types::Text].include?(property.type)
-          #len = property.length  # XXX: maybe length should always return a Range, with the min defaulting to 1
-          len = property.options.fetch(:length, property.options.fetch(:size, DataMapper::Property::DEFAULT_LENGTH))
-          if len.is_a?(Range)
-            opts[:within] = len
-          else
-            opts[:maximum] = len
-          end
-          # validates_length property.name, opts
-          validates_length property.name, options_with_message(opts, property, :length)
-        end
-
-        # format
-        if property.options.has_key?(:format)
-          opts[:with] = property.options[:format]
-          # validates_format property.name, opts
-          validates_format property.name, options_with_message(opts, property, :format)
-        end
-
-        # uniqueness validator
-        if property.options.has_key?(:unique)
-          value = property.options[:unique]
-          if value.is_a?(Array) || value.is_a?(Symbol)
-            # validates_is_unique property.name, :scope => Array(value)
-            validates_is_unique property.name, options_with_message({:scope => Array(value)}, property, :is_unique)
-          elsif value.is_a?(TrueClass)
-            # validates_is_unique property.name
-            validates_is_unique property.name, options_with_message({}, property, :is_unique)
-          end
-        end
-
-        # within validator
-        if property.options.has_key?(:set)
-          validates_within property.name, options_with_message({:set => property.options[:set]}, property, :within)
-        end
-
-        # numeric validator
-        if Integer == property.type
-          opts[:integer_only] = true
-          # validates_is_number property.name, opts
-          validates_is_number property.name, options_with_message(opts, property, :is_number)
-        elsif BigDecimal == property.type || Float == property.type
-          opts[:precision] = property.precision
-          opts[:scale]     = property.scale
-          # validates_is_number property.name, opts
-          validates_is_number property.name, options_with_message(opts, property, :is_number)
-        else
-          # We only need this in the case we don't already
-          # have a numeric validator, because otherwise
-          # it will cause duplicate validation errors
-          unless property.custom?
-            # validates_is_primitive property.name, opts
-            validates_is_primitive property.name, options_with_message(opts, property, :is_primitive)
-          end
-        end
+        infer_presence_validation_for(property, opts.dup)
+        infer_length_validation_for(property, opts.dup)
+        infer_format_validation_for(property, opts.dup)
+        infer_uniqueness_validation_for(property, opts.dup)
+        infer_within_validation_for(property, opts.dup)
+        infer_numeric_validation_for(property, opts.dup)
       end # auto_generate_validations
 
       # Checks whether auto validations are currently
@@ -175,6 +119,66 @@ module DataMapper
       #   true for properties with :auto_validation option that has positive value
       def skip_auto_validation_for?(property)
         property.options.has_key?(:auto_validation) && !property.options[:auto_validation]
+      end
+
+      def infer_presence_validation_for(property, options)
+        unless property.nullable? || property.serial?
+          # validates_present property.name, opts
+          validates_present property.name, options_with_message(options, property, :presence)
+        end
+      end
+
+      def infer_length_validation_for(property, options)
+        if [String, DataMapper::Types::Text].include?(property.type)
+          len = property.options.fetch(:length, property.options.fetch(:size, DataMapper::Property::DEFAULT_LENGTH))
+          if len.is_a?(Range)
+            options[:within] = len
+          else
+            options[:maximum] = len
+          end
+
+          validates_length property.name, options_with_message(options, property, :length)
+        end
+      end
+
+      def infer_format_validation_for(property, options)
+        if property.options.has_key?(:format)
+          options[:with] = property.options[:format]
+          validates_format property.name, options_with_message(options, property, :format)
+        end
+      end
+
+      def infer_uniqueness_validation_for(property, options)
+        if property.options.has_key?(:unique)
+          value = property.options[:unique]
+          if value.is_a?(Array) || value.is_a?(Symbol)
+            validates_is_unique property.name, options_with_message({:scope => Array(value)}, property, :is_unique)
+          elsif value.is_a?(TrueClass)
+            validates_is_unique property.name, options_with_message({}, property, :is_unique)
+          end
+        end
+      end
+
+      def infer_within_validation_for(property, options)
+        validates_within property.name, options_with_message({:set => property.options[:set]}, property, :within) if property.options.has_key?(:set)
+      end
+
+      def infer_numeric_validation_for(property, options)
+        if Integer == property.type
+          options[:integer_only] = true
+
+          validates_is_number property.name, options_with_message(options, property, :is_number)
+        elsif BigDecimal == property.type || Float == property.type
+          options[:precision] = property.precision
+          options[:scale]     = property.scale
+
+          validates_is_number property.name, options_with_message(options, property, :is_number)
+        else
+          # We only need this in the case we don't already
+          # have a numeric validator, because otherwise
+          # it will cause duplicate validation errors
+          validates_is_primitive property.name, options_with_message(options, property, :is_primitive) unless property.custom?
+        end
       end
     end # module AutoValidate
   end # module Validate
