fb4b1be0b0f91b40cceee6ad6a0f73967e2b8df0
Michael S. Klishin <michael@novemberain.com>
2009-03-31 23:31:45 +0400

    [dm-validations] Encourage people use method validator with one property/method at a time
     * See comments to http://datamapper.lighthouseapp.com/projects/20609-datamapper/tickets/752

--- dm-validations/lib/dm-validations/validators/method_validator.rb
+++ dm-validations/lib/dm-validations/validators/method_validator.rb
@@ -26,30 +26,38 @@ module DataMapper
     module ValidatesWithMethod
 
       ##
-      # Validate using the given method. The method must to return
-      # either true, or a pair of [false, error message string]
+      # Validate using method called on validated object. The method must to return
+      # either true, or a pair of [false, error message string], and is specified
+      # as a symbol passed with :method option.
+      #
+      # This validator does support multiple fields being specified at a time,
+      # but we encourage you to use it with one property/method at a time.
+      #
+      # Real world experience shows that method validation is often useful when
+      # attribute needs to be virtual and not a property name.
       #
       # @example [Usage]
       #   require 'dm-validations'
       #
-      #   class Page
-      #     include DataMapper::Resource
+      #  class Page
+      #    include DataMapper::Resource
       #
-      #     property :zip_code, String
+      #    property :zip_code, String
       #
-      #     validates_with_method :in_the_right_location?
+      #    validates_with_method :zip_code, :method => :in_the_right_location?
       #
-      #     def in_the_right_location?
-      #       if @zip_code == "94301"
-      #         return true
-      #       else
-      #         return [false, "You're in the wrong zip code"]
-      #       end
-      #     end
+      #    def in_the_right_location?
+      #      if @zip_code == "94301"
+      #        return true
+      #      else
+      #        return [false, "You're in the wrong zip code"]
+      #      end
+      #    end
       #
-      #     # A call to valid? will return false and
-      #     # populate the object's errors with "You're in the
-      #     # wrong zip code" unless zip_code == "94301"
+      #    # A call to valid? will return false and
+      #    # populate the object's errors with "You're in the
+      #    # wrong zip code" unless zip_code == "94301"
+      #  end
       def validates_with_method(*fields)
         opts = opts_from_validator_args(fields)
         add_validator_to_context(opts, fields, DataMapper::Validate::MethodValidator)
