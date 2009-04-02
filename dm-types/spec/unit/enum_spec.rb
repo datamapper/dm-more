67dee9327c5fcae79088de2f888a86b0a0911b28
Michael S. Klishin <michael@novemberain.com>
2009-04-02 07:14:00 +0400

    [dm-types] Spec examples in spec/unit/enum actually explain what they do now

--- dm-types/spec/unit/enum_spec.rb
+++ dm-types/spec/unit/enum_spec.rb
@@ -4,29 +4,27 @@ require Pathname(__FILE__).dirname.parent.expand_path + 'spec_helper'
 describe DataMapper::Types::Enum do
 
   describe ".new" do
-    before :all do
-
-    end
-    it "should create a Class" do
+    it "returns a Class" do
       DataMapper::Types::Enum.new.should be_instance_of(Class)
     end
 
-    it "should create unique a Class each call" do
+    it "returns an unique Class each time" do
       DataMapper::Types::Enum.new.should_not == DataMapper::Types::Enum.new
     end
 
-    it "should use the arguments as the values in the @flag_map hash" do
+    it "stores enumeration items" do
       DataMapper::Types::Enum.new(:first, :second, :third).flag_map.values.should == [:first, :second, :third]
     end
 
-    it "should create incremental keys for the @flag_map hash, staring at 1" do
+    it "assigns incremental numeric keys to enumeration items" do
       DataMapper::Types::Enum.new(:one, :two, :three, :four).flag_map.keys.should == (1..4).to_a
     end
 
-    it "should have an Integer primitive type" do
+    it "uses Integer as a primitive" do
       DataMapper::Types::Enum.new.primitive.should == Integer
     end
   end
+
 
   describe ".dump" do
     before(:each) do
@@ -46,23 +44,25 @@ describe DataMapper::Types::Enum do
     end
   end
 
+
   describe ".load" do
     before(:each) do
       @enum = DataMapper::Types::Enum[:uno, :dos, :tres]
     end
 
-    it "should return the value of the key match from the flag map" do
+    it "returns the value of the key match from the flag map" do
       @enum.load(1, :property).should == :uno
       @enum.load(2, :property).should == :dos
       @enum.load(3, :property).should == :tres
     end
 
     describe "when there is no key" do
-      it "should return nil" do
+      it "returns nil" do
         @enum.load(-1, :property).should be_nil
       end
     end
   end
+
 
   describe ".typecast" do
     describe "of Enum created from a symbol" do
