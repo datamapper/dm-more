1f2a2f183b66724de8516d91378fa91773bc3039
Michael S. Klishin <michael@novemberain.com>
2009-04-04 19:15:51 +0400

    [dm-types] Forgot this new fixture file

---
+++ dm-types/spec/fixtures/ticket.rb
@@ -0,0 +1,25 @@
+module DataMapper
+  module Types
+    module Fixtures
+      class Ticket
+        #
+        # Behaviors
+        #
+
+        include DataMapper::Resource
+        include DataMapper::Validate
+
+        #
+        # Properties
+        #
+
+        property :id,     Serial
+        property :title,  String, :length => 255
+        property :body,   Text
+        property :status, Enum[:unconfirmed, :confirmed, :assigned, :resolved, :not_applicable]
+      end # Ticket
+
+      Ticket.auto_migrate!
+    end
+  end
+end
