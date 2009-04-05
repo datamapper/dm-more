2bac0a2595dc441c67caf7c3de895fb15a6a7378
Michael S. Klishin <michael@novemberain.com>
2009-04-05 13:46:38 +0400

    [dm-types] Reworked integration spec exampes for Flag type

---
+++ dm-types/spec/fixtures/tshirt.rb
@@ -0,0 +1,28 @@
+module DataMapper
+  module Types
+    module Fixtures
+
+      class TShirt
+        #
+        # Behaviors
+        #
+
+        include ::DataMapper::Resource
+
+        #
+        # Properties
+        #
+
+        property :id,          Serial
+        property :writing,     String
+        property :has_picture, Boolean, :default => false
+        property :picture,     Enum[:octocat, :fork_you, :git_down]
+
+        property :color,   Enum[:white, :black, :red, :orange, :yellow, :green, :cyan, :blue, :purple]
+        property :size,    Flag[:xs, :small, :medium, :large, :xl, :xxl]
+      end # Shirt
+
+      TShirt.auto_migrate!
+    end # Fixtures
+  end # Types
+end # DataMapper
--- dm-types/spec/integration/flag_spec.rb
+++ dm-types/spec/integration/flag_spec.rb
@@ -1,35 +1,61 @@
 require 'pathname'
 require Pathname(__FILE__).dirname.parent.expand_path + 'spec_helper'
 
-describe DataMapper::Types::Flag do
-  before(:all) do
-    class ::Shirt
-      include DataMapper::Resource
-
-      property :id,    Serial
-      property :sizes, Flag[:xs, :small, :medium, :large, :xl, :xxl]
-    end
-
-    Shirt.auto_migrate!
+describe DataMapper::Types::Fixtures::TShirt do
+  before :each do
+    @model = DataMapper::Types::Fixtures::TShirt.new(:writing     => "Fork you",
+                                                     :has_picture => true,
+                                                     :picture     => :octocat,
+                                                     :color       => :white,
+                                                     :size        => [:xs, :medium])
   end
 
-  it "should save with create({:flag_field => [:flags]})" do
-    lambda { Shirt.create(:sizes => [:medium, :large]) }.should_not raise_error
-    DataMapper.repository do
-      Shirt.get(1).sizes.should == [:medium, :large]
-    end
-  end
+  describe "with multiple sizes" do
+    describe "dumped and loaded" do
+      before :each do
+        @model.save.should be_true
+        @model.reload
+      end
 
-  it "should save with flag_field=[:flags]" do
-    shirt = Shirt.new
-    shirt.sizes = [:small, :xs]
-    lambda { shirt.save }.should_not raise_error
-    DataMapper.repository do
-      Shirt.get(2).sizes.should == [:xs, :small]
+      it "returns size as array" do
+        @model.size.should == [:xs, :medium]
+      end
     end
   end
 
-  it 'should immediately typecast supplied values' do
-    Shirt.new(:sizes => [:large, :xl]).sizes.should == [:large, :xl]
+
+  describe "with a single size" do
+    before :each do
+      @model.size = :large
+    end
+
+    describe "dumped and loaded" do
+      before :each do
+        @model.save.should be_true
+        @model.reload
+      end
+
+      it "returns size as array with a single value" do
+        @model.size.should == [:large]
+      end
+    end
+  end
+
+
+
+  # Flag does not add any auto validations
+  describe "without size" do
+    before :each do
+      @model.should be_valid
+      @model.size = nil
+    end
+
+    it "is valid" do
+      @model.should be_valid
+    end
+
+    it "has no errors" do
+      @model.errors.should be_blank
+    end
   end
 end
