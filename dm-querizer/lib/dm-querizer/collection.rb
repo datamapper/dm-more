0d032253bd9dfe8624b9edba14b417c6f13c993a
Dan Kubb <dan.kubb@gmail.com>
2009-05-14 10:51:59 -0700

    [dm-querizer] Updated to pass specs with dm-core 0.10.0

--- dm-querizer/lib/dm-querizer/collection.rb
+++ dm-querizer/lib/dm-querizer/collection.rb
@@ -1,10 +1,11 @@
 module DataMapper
   class Collection
-    def all(query = {},&block)
+    alias original_all all
+
+    def all(query={}, &block)
       query = DataMapper::Querizer.translate(&block) if block
-      return self if query.kind_of?(Hash) ? query.empty? : query == self.query
       query = scoped_query(query)
-      query.repository.read_many(query)
+      original_all(query)
     end
   end
 end
