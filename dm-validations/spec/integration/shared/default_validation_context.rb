2393a5db4477067cde18af3de01ab9c138c5575c
snusnu <gamsnjaga@gmail.com>
2009-06-30 17:26:49 +0200

    [dm-validations] corrected spec expectation wording

--- dm-validations/spec/integration/shared/default_validation_context.rb
+++ dm-validations/spec/integration/shared/default_validation_context.rb
@@ -6,7 +6,7 @@ describe "object invalid in default context", :shared => true do
 end
 
 describe "object valid in default context", :shared => true do
-  it "is not valid in default context" do
+  it "is valid in default context" do
     @model.should be_valid
     @model.should be_valid(:default)
   end
