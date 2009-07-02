69089e063a9164a98972edb927390d52298fc0c5
Dan Kubb <dan.kubb@gmail.com>
2009-07-02 08:51:20 -0700

    [dm-constraints] Update to work with Model::DescendantSet

--- dm-constraints/spec/spec_helper.rb
+++ dm-constraints/spec/spec_helper.rb
@@ -31,15 +31,17 @@ load_driver(:mysql,    'mysql://localhost/dm_core_test')
 Spec::Runner.configure do |config|
   config.after :all do
     # global model cleanup
-    descendants = DataMapper::Model.descendants.dup.to_a
+    descendants = DataMapper::Model.descendants.to_a
     while model = descendants.shift
-      descendants.concat(model.descendants) if model.respond_to?(:descendants)
+      descendants.concat(model.descendants.to_a - [ model ])
 
       parts         = model.name.split('::')
       constant_name = parts.pop.to_sym
       base          = parts.empty? ? Object : Object.full_const_get(parts.join('::'))
 
-      base.send(:remove_const, constant_name)
+      if base.const_defined?(constant_name)
+        base.send(:remove_const, constant_name)
+      end
 
       DataMapper::Model.descendants.delete(model)
     end
