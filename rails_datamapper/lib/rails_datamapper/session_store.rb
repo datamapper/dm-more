7e0a9ea0381ad1997678430125175f21408e711f
Jordan Ritter <jpr5@darkridge.com>
2009-07-23 09:59:06 -0700

    Fix bug whereby my changed Session model (adding id as PK) caused all Session.get's to never get the session data back.  Updated the model to change :session_id to :unique_index, and changed all Session.get()s to Session.first()s.

--- rails_datamapper/lib/rails_datamapper/session_store.rb
+++ rails_datamapper/lib/rails_datamapper/session_store.rb
@@ -24,12 +24,13 @@ module ActionController
       private
       def get_session(env, sid)
         sid     ||= generate_sid
-        session   = @@session_class.get(sid)
+        session   = @@session_class.first(:session_id => sid)
         [sid, session.nil? ? {} : session.data]
       end
 
       def set_session(env, sid, session_data)
-        session            = @@session_class.get(sid) || @@session_class.new(:session_id => sid)
+        session            = @@session_class.first(:session_id => sid) ||
+                             @@session_class.new(:session_id => sid)
         session.data       = session_data || {}
         session.updated_at = Time.now if session.dirty?
 
@@ -48,7 +49,7 @@ module DataMapperStore
     end
 
     property :id,         Serial
-    property :session_id, String,   :key => true
+    property :session_id, String,   :unique_key => true
     property :data,       Text,     :nullable => false, :default => ::Base64.encode64(Marshal.dump({}))
     property :updated_at, DateTime, :nullable => true, :index => true
 
