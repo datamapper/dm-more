8c05a03c1b7325614d345d3e2e0386c9c4c09c04
Dan Kubb <dan.kubb@gmail.com>
2009-09-11 11:25:57 -0700

    [dm-validations] Refactored length validator
    * Removed unecessary usage of Range#max from LengthValidator#call which
      was causing some slowness when the max was large (due to how Range#max
      uses Enumerable#max under the hood)
    * Added YARD docs for all refactored methods

--- dm-validations/lib/dm-validations/validators/length_validator.rb
+++ dm-validations/lib/dm-validations/validators/length_validator.rb
@@ -1,81 +1,166 @@
 module DataMapper
   module Validate
-
-    ##
-    #
-    # @author Guy van den Berg
-    # @since  0.9
     class LengthValidator < GenericValidator
 
+      # Initialize a length validator
+      #
+      # @param [Symbol] field_name
+      #   the name of the field to validate
+      # @param [Hash] options
+      #   the validator options
+      #
+      # @return [undefined]
+      #
+      # @api semipublic
       def initialize(field_name, options)
         super
 
-        @min = options[:minimum] || options[:min]
-        @max = options[:maximum] || options[:max]
-        @equal = options[:is] || options[:equals]
-        @range = options[:within] || options[:in]
+        @equal = options[:is]      || options[:equals]
+        @range = options[:within]  || options[:in]
+        @min   = options[:minimum] || options[:min]
+        @max   = options[:maximum] || options[:max]
 
-        if !@range && @min && @max
-          @range = Range.new(@min, @max)
+        if @min && @max
+          @range ||= @min..@max
         end
-
-        @validation_method ||= :range if @range
-        @validation_method ||= :min if @min && @max.nil?
-        @validation_method ||= :max if @max && @min.nil?
-        @validation_method ||= :equals unless @equal.nil?
       end
 
+      # Test the resource field for validity
+      #
+      # @example when the resource field is valid
+      #   validator.call(valid_resource)  # => true
+      #
+      # @example when the resource field is not valid
+      #   validator.call(invalid_resource)  # => false
+      #
+      #
+      # @param [Resource] target
+      #   the Resource to test
+      #
+      # @return [Boolean]
+      #   true if the field is valid, false if not
+      #
+      # @api semipublic
       def call(target)
-        field_value = target.validation_property_value(field_name)
-        return true if @options[:allow_nil] && field_value.blank?
+        value = target.validation_property_value(field_name)
+        return true if allow_nil? && value.blank?
 
-        field_value = '' if field_value.nil?
+        return true unless error_message = error_message_for(value)
 
-        min = @range ? @range.min : @min
-        max = @range ? @range.max : @max
-        equal = @equal
+        add_error(target, error_message, field_name)
 
-        field_value_length = field_value_length(field_value)
-
-        case @validation_method
-        when :range then
-          unless valid = @range.include?(field_value_length)
-            error_message = ValidationErrors.default_error_message(:length_between, humanized_field_name, min, max)
-          end
-        when :min then
-          unless valid = field_value_length >= min
-            error_message = ValidationErrors.default_error_message(:too_short, humanized_field_name, min)
-          end
-        when :max then
-          unless valid = field_value_length <= max
-            error_message = ValidationErrors.default_error_message(:too_long, humanized_field_name, max)
-          end
-        when :equals then
-          unless valid = field_value_length == equal
-            error_message = ValidationErrors.default_error_message(:wrong_length, humanized_field_name, equal)
-          end
-        end
-
-        error_message = @options[:message] || error_message
-
-        add_error(target, error_message, field_name) unless valid
-
-        return valid
+        false
       end
 
       private
 
+      # Return if the validation allows nil values
+      #
+      # @return [Boolean]
+      #   true if the validation allows nil, false if not
+      #
+      # @api private
+      def allow_nil?
+        options.fetch(:allow_nil, false)
+      end
+
+      # Return the error messages for the value if it is invalid
+      #
+      # @param [#to_s] value
+      #   the value to test
+      #
+      # @return [String, nil]
+      #   the error message if invalid, nil if not
+      #
+      # @api private
+      def error_message_for(value)
+        if error_message = send(validation_method, value_length(value.to_s))
+          @options.fetch(:message, error_message)
+        end
+      end
+
+      # Return the method to validate the value with
+      #
+      # @return [Symbol]
+      #   the validation method
+      #
+      # @api private
+      def validation_method
+        @validation_method ||=
+          if    @equal then :validate_equals
+          elsif @range then :validate_range
+          elsif @min   then :validate_min
+          elsif @max   then :validate_max
+          end
+      end
+
       # Return the length in characters
       #
-      # @param [#to_str] field_value
+      # @param [#to_str] value
       #   the string to get the number of characters for
       #
       # @return [Integer]
       #   the number of characters in the string
       #
       # @api private
-      def field_value_length(field_value)
-        field_value.to_str.split(//).size
+      def value_length(value)
+        value.to_str.split(//).size
+      end
+
+      # Validate the value length is equal to the expected length
+      #
+      # @param [Integer] length
+      #   the value length
+      #
+      # @return [String, nil]
+      #   the error message if invalid, nil if not
+      #
+      # @api private
+      def validate_equals(length)
+        return if length == @equal
+        ValidationErrors.default_error_message(:wrong_length, humanized_field_name, @equal)
+      end
+
+      # Validate the value length is within expected range
+      #
+      # @param [Integer] length
+      #   the value length
+      #
+      # @return [String, nil]
+      #   the error message if invalid, nil if not
+      #
+      # @api private
+      def validate_range(length)
+        return if @range.include?(length)
+        ValidationErrors.default_error_message(:length_between, humanized_field_name, @range.min, @range.max)
+      end
+
+      # Validate the minimum expected value length
+      #
+      # @param [Integer] length
+      #   the value length
+      #
+      # @return [String, nil]
+      #   the error message if invalid, nil if not
+      #
+      # @api private
+      def validate_min(length)
+        return if length >= @min
+        ValidationErrors.default_error_message(:too_short, humanized_field_name, @min)
+      end
+
+      # Validate the maximum expected value length
+      #
+      # @param [Integer] length
+      #   the value length
+      #
+      # @return [String, nil]
+      #   the error message if invalid, nil if not
+      #
+      # @api private
+      def validate_max(length)
+        return if length <= @max
+        ValidationErrors.default_error_message(:too_long, humanized_field_name, @max)
       end
 
     end # class LengthValidator
