26e11cab84aff993e866a275246381108718b6f9
Dan Kubb <dan.kubb@gmail.com>
2009-10-06 10:00:42 -0700

    [dm-types] Decreased the Slug maximum length to something reasonable

--- dm-types/lib/dm-types/slug.rb
+++ dm-types/lib/dm-types/slug.rb
@@ -4,7 +4,11 @@ module DataMapper
   module Types
     class Slug < DataMapper::Type
       primitive String
-      length    65535
+      length    2000
+
+      # Maximum length chosen because URI type is limited to 2000
+      # characters, and a slug is a component of a URI, so it should
+      # not exceed the maximum URI length either.
 
       def self.load(value, property)
         value
