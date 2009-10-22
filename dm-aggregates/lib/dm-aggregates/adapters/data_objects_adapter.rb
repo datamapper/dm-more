22f2cfe1b15f6feaae17182074e2b87b7a0a691f
Dan Kubb <dan.kubb@gmail.com>
2009-10-21 22:18:40 -0700

    [dm-aggregates] Fixed failing specs due to changes in dm-core for do_sqlserver

--- dm-aggregates/lib/dm-aggregates/adapters/data_objects_adapter.rb
+++ dm-aggregates/lib/dm-aggregates/adapters/data_objects_adapter.rb
@@ -72,24 +72,24 @@ module DataMapper
           RUBY
         end
 
-        def property_to_column_name_with_operator(property, qualify)
+        def property_to_column_name_with_operator(property, qualify, qualifier = nil)
           case property
             when DataMapper::Query::Operator
-              aggregate_field_statement(property.operator, property.target, qualify)
+              aggregate_field_statement(property.operator, property.target, qualify, qualifier)
 
             when Property, DataMapper::Query::Path
-              property_to_column_name_without_operator(property, qualify)
+              property_to_column_name_without_operator(property, qualify, qualifier)
 
             else
               raise ArgumentError, "+property+ must be a DataMapper::Query::Operator, a DataMapper::Property or a Query::Path, but was a #{property.class} (#{property.inspect})"
           end
         end
 
-        def aggregate_field_statement(aggregate_function, property, qualify)
+        def aggregate_field_statement(aggregate_function, property, qualify, qualifier)
           column_name = if aggregate_function == :count && property == :all
             '*'
           else
-            property_to_column_name(property, qualify)
+            property_to_column_name(property, qualify, qualifier)
           end
 
           function_name = case aggregate_function
